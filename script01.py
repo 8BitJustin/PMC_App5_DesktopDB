f7a39c2864adc32b715054c2276090dc3cba6fd8
8bitjustin <j.olson.digital@gmail.com>
2019-08-09 14:28:26 -0700

    Added buttons (non-functional).

--- script01.py
+++ script01.py
@@ -2,6 +2,8 @@ from tkinter import *
 
 window = Tk()
 
+# Labels
+
 l1 = Label(window,text="Title")
 l1.grid(row=0, column=0)
 
@@ -13,6 +15,8 @@ l3.grid(row=1, column=0)
 
 l4 = Label(window,text="ISBN")
 l4.grid(row=1, column=2)
+
+# Input entries
 
 title_text = StringVar()
 e1 = Entry(window,textvariable=title_text)
@@ -30,6 +34,8 @@ isbn_text = StringVar()
 e4 = Entry(window,textvariable=isbn_text)
 e4.grid(row=1, column=3)
 
+# Listbox and Scrollbar
+
 list1 = Listbox(window, height=6, width=34)
 list1.grid(row=2, column=0, rowspan=6, columnspan=2)
 
@@ -39,31 +45,24 @@ sb1.grid(row=2, column=2, rowspan=6)
 list1.configure(yscrollcommand=sb1.set)
 sb1.configure(command=list1.yview)
 
+# Buttons
+
+b1 = Button(window, text="View", width=12)
+b1.grid(row=2, column=3)
+
+b2 = Button(window, text="Search", width=12)
+b2.grid(row=3, column=3)
+
+b3 = Button(window, text="Add", width=12)
+b3.grid(row=4, column=3)
+
+b4 = Button(window, text="Update", width=12)
+b4.grid(row=5, column=3)
+
+b5 = Button(window, text="Delete", width=12)
+b5.grid(row=6, column=3)
+
+b6 = Button(window, text="Close", width=12)
+b6.grid(row=7, column=3)
+
 window.mainloop()
-
-
-
-
-
-
-
-
-
-
-
-
-"""
-A program that stores this book information:
-Title
-Author
-Year
-ISBN
-
-User can:
-View all records
-Search an entry
-Add entry
-Update entry
-Delete entry
-Close
-"""
